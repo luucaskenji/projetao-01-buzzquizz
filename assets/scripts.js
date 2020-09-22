a499084dd1e53974311b0bb76ec7eb2ee353e798
Lucas Murakami <lkenjim98@gmail.com>
2020-09-22 17:14:48 -0300

    Autenticação do usuário

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,7 @@
+var button = document.querySelector(".submit a");
+var userToken;
+
 function verifyInputContent() {
-    var button = document.querySelector(".submit a");
     button.style.pointerEvents="none";
 
     var inputEMail = document.querySelector(".email-input").value;
@@ -13,4 +15,22 @@ function verifyInputContent() {
     else {
         postServerRequest(inputEMail, inputPassword);
     }
+}
+
+function postServerRequest(email, password) {
+    objPost = { email: email, password: password }
+    var request = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v1/buzzquizz/users", objPost);
+    request.then(displayQuizzesList).catch(startAllOver);
+}
+
+function displayQuizzesList(serverResponse) {
+    document.querySelector(".login").style.display = "none";
+    document.querySelector(".quizzes-list").style.display = "flex";
+
+    userToken = serverResponse.data.token;
+}
+
+function startAllOver(serverResponse) {
+    alert("Email e/ou senha incorretos");
+    button.style.pointerEvents = "initial";
 }
