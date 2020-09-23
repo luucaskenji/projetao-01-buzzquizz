25105d5c601f84751598a15317c5d93ffb3aeb4c
Lucas Murakami <lkenjim98@gmail.com>
2020-09-23 02:34:17 -0300

    Criação de múltiplas perguntas (sem enviar ao servidor)

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,7 @@
-var button = document.querySelector(".submit a");
+var button = document.querySelector(".authentication");
 var userToken;
+var numberOfQuestions=1, numberOfLevels=1;
+var quizzTitle, quizzQuestion, rightAnswer, wrongAnswer;
 
 function verifyInputContent() {
     button.style.pointerEvents="none";
@@ -38,7 +40,57 @@ function startAllOver(serverResponse) {
     button.style.pointerEvents = "initial";
 }
 
-function quizzCreation() {
+function displayQuizzCreation() {
     document.querySelector(".quizzes-list").style.display = "none";
     document.querySelector(".quizz-creation").style.display = "flex";
+}
+
+function addQuestion() {
+    numberOfQuestions++;
+    
+    var newQuestion = document.createElement("div");
+    newQuestion.classList.add("question-answers");
+    newQuestion.innerHTML = "<span>Pergunta " + numberOfQuestions + "</span><input type='text' class='question' placeholder='Digite a pergunta'><div class='answers-images'><input type='text' placeholder='Digite a resposta correta' class='correct right-answer'><input type='text' placeholder='Link pra imagem correta' class='correct right-image-src'><input type='text' placeholder='Digite uma resposta errada 1' class='wrong wrong-answer'><input type='text' placeholder='Link pra imagem errada 1' class='wrong wrong-image-src'><input type='text' placeholder='Digite uma resposta errada 2' class='wrong wrong-answer'><input type='text' placeholder='Link pra imagem errada 2' class='wrong wrong-image-src'><input type='text' placeholder='Digite uma resposta errada 3' class='wrong wrong-answer'><input type='text' placeholder='Link pra imagem errada 3' class='wrong wrong-image-src'></div>";
+
+    document.querySelector(".questions-container").appendChild(newQuestion);
+}
+
+function createQuizz() {
+    var createdQuizzObject = {
+        title: "",
+        data: {
+            questions: []
+        }
+    };
+
+    objectQuestions = createdQuizzObject.data.questions;
+    var titleInput = document.querySelector(".quizz-title").value;
+    createdQuizzObject.title = titleInput;
+
+    for (var i=1; i<=numberOfQuestions; i++) {
+        var possibleAnswers = [];
+        var imagesSources = [];
+        var newQuestionPosition = { question: "", answers: [], images: [] };
+        objectQuestions.push(newQuestionPosition);
+
+        var elementOrder = document.querySelector(".question-answers:nth-child(" + i + ")");
+
+        var questionInput = elementOrder.querySelector(".question").value;
+        var correctAnswer = elementOrder.querySelector(".right-answer").value;
+        var correctImage = elementOrder.querySelector(".right-image-src").value;
+        var arrayWrongAnswers = elementOrder.querySelectorAll(".wrong-answer");
+        var arrayWrongImagesSources = elementOrder.querySelectorAll(".wrong-image-src");
+
+        possibleAnswers.push(correctAnswer);
+        for (var j=0; j<=2; j++) possibleAnswers.push(arrayWrongAnswers[j].value);
+
+        imagesSources.push(correctImage);
+        for (var j=0; j<=2; j++) imagesSources.push(arrayWrongImagesSources[j].value);        
+
+        objectQuestions[i-1].question = questionInput;
+        objectQuestions[i-1].answers = possibleAnswers;
+        objectQuestions[i-1].images = imagesSources;
+    }
+
+    console.log(createdQuizzObject);
 }
